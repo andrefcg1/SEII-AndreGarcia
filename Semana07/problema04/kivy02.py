eec5198d4d9929b79ce08d5261c1d03ea38aacec
andrefcg1 <65246453+andrefcg1@users.noreply.github.com>
2022-06-22 18:24:32 -0300

    ultima parte da atividade

---
+++ Semana07/problema04/kivy02.py
@@ -0,0 +1,13 @@
+from kivy.app import App
+from kivy.uix.gridlayout import GridLayout
+from kivy.uix.label import Label
+from kivy.uix.image import Image
+from kivy.uix.button import Button
+from kivy.uix.textinput import TextInput
+from kivy.uix.widget import Widget
+from kivy.uix.boxlayout import BoxLayout
+
+class LanguageLearner(App):
+    pass
+
+LanguageLearner().run()
